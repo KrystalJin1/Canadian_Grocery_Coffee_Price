58b8affe5406cb00bf9708cdd32558c33434c050
Jin Zhang <kry.zhang@mail.utoronto.ca>
2024-12-01 01:23:02 -0500

    Update the scripts
    Finished the basic setup of the scripts. Finished the data cleaning.

---
+++ scripts/05-model_data.R
@@ -0,0 +1,37 @@
+#### Preamble ####
+# Purpose: Development and validation of a Bayesian regression model for predicting coffee product pricing based on historical data
+# Author: Jin Zhang
+# Date: today
+# Contact: kry.zhang@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: R, rstanarm package, and arrow package for data handling
+# Any other information needed? Ensure rstanarm and arrow are properly installed and configured before running this script.
+
+
+
+
+#### Workspace setup ####
+library(tidyverse)
+library(rstanarm)
+library(arrow)
+
+#### Read data ####
+analysis_data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
+
+# Set the final coffee product pricing model
+Coffee_product_pricing <-
+  stan_glm(
+    formula = current_price ~ old_price + month + vendor,
+    data = analysis_data,
+    family = gaussian(),
+    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
+    prior_aux = exponential(rate = 1, autoscale = TRUE),
+    seed = 1008184288
+  )
+
+#### Save model ####
+saveRDS(
+  Coffee_product_pricing,
+  file = "models/Coffee_product_pricing.rds"
+)
